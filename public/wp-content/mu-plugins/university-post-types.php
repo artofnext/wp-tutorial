8e89f2329a3449f04e94d1e0fd5eae4441383448
artofnext <novnovalex@gmail.com>
2020-04-16 14:32:40 +0300

    custom post label properties

--- public/wp-content/mu-plugins/university-post-types.php
+++ public/wp-content/mu-plugins/university-post-types.php
@@ -1,8 +1,13 @@
+<?php
 function university_post_types() {
     register_post_type('event', array(
         'public' => true,
         'labels' => array(
             'name' => 'Events',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event',
         ),
         'menu_icon' => 'dashicons-calendar',
     ));
