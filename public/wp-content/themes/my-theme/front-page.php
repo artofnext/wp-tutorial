8ef3ef40f41557bc2c53fea02a061602b2e17219
artofnext <novnovalex@gmail.com>
2020-05-06 17:23:13 +0300

    order posts

--- public/wp-content/themes/my-theme/front-page.php
+++ public/wp-content/themes/my-theme/front-page.php
@@ -17,8 +17,10 @@
 
       <?php
       $homepageEvents = new WP_Query(array(
-        'posts_per_page' => 2,
+        'posts_per_page' => -1,
         'post_type' => 'event',
+        'orderby' => 'title',
+        'order' => 'ASC',
       ));
 
       while ($homepageEvents->have_posts()) {
