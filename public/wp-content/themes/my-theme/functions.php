a4db62dc7640005f60092e69b1502b0f793f249a
artofnext <novnovalex@gmail.com>
2020-05-08 17:39:14 +0300

    add filter & order to main query

--- public/wp-content/themes/my-theme/functions.php
+++ public/wp-content/themes/my-theme/functions.php
@@ -21,7 +21,19 @@ add_action('after_setup_theme', 'universaty_features');
 
 function university_adjust_queries($query) {
     if(!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
-        $query->set('posts_per_page', '1');
+        $today = date('Ymd');
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            array(
+              'key' => 'event_date',
+              'compare' => '>=',
+              'value' => $today,
+              'type' => 'numeric',
+            ),
+          ),
+        );
     }
 }
 
