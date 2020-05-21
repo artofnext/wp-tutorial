49a7c923183bbbbbd83ea906c0f4d3fa3fe812bf
artofnext <novnovalex@gmail.com>
2020-05-21 12:18:23 +0300

    add thumbnail regenerator plugin

--- public/wp-content/themes/my-theme/functions.php
+++ public/wp-content/themes/my-theme/functions.php
@@ -14,6 +14,9 @@ function universaty_features() {
     // register_nav_menu('footerMenuLocation2', 'Footer Menu Location 2');
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+    add_image_size('professorLandscape', 400, 260, true);
+    add_image_size('professorPortrait', 480, 650, true);
+    
 }
 
 add_action('wp_enqueue_scripts','university_files');
