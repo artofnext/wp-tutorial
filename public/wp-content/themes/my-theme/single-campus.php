04c9c65bcb8a3528d7e30de0e8dc58691bc588ed
artofnext <novnovalex@gmail.com>
2020-06-02 17:33:38 +0300

    add related field programs

--- public/wp-content/themes/my-theme/single-campus.php
+++ public/wp-content/themes/my-theme/single-campus.php
@@ -34,33 +34,30 @@ while (have_posts()) {
 
         <?php
 
-        $relatedProfessors = new WP_Query(array(
+        $relatedPrograms = new WP_Query(array(
             'posts_per_page' => -1,
-            'post_type' => 'professor',
+            'post_type' => 'program',
             'orderby' => 'title',
             'order' => 'ASC',
             'meta_query' => array(
                 array(
-                    'key' => 'related_programs',
+                    'key' => 'related_campus',
                     'compare' => 'LIKE',
                     'value' => '"' . get_the_ID() . '"', // for search in PHP serialised array as a string
                 ),
             ),
         ));
 
-        if ($relatedProfessors->have_posts()) {
+        if ($relatedPrograms->have_posts()) {
             echo '<hr class="section-break">';
-            echo '<h2 class="headline headline--medium"> ' . get_the_title() . ' Professrs</h2>';
+            echo '<h2 class="headline headline--medium">Programs avaliable at campus</h2>';
 
 
-            echo '<ul class="professor-cards">';
-            while ($relatedProfessors->have_posts()) {
-                $relatedProfessors->the_post(); ?>
-                <li class="professor-card__list-item">
-                    <a class="professor-card" href="<?php the_permalink(); ?>">
-                        <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>">
-                        <span class="professor-card__name"><?php the_title(); ?></span>
-                    </a>
+            echo '<ul class="min-list link-list">';
+            while ($relatedPrograms->have_posts()) {
+                $relatedPrograms->the_post(); ?>
+                <li >
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                 </li>
         <?php
             }
